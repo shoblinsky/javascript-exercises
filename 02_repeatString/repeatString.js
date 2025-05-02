f00fae4dcc4e7f9ad35c315275f0713f47307df3
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-02 21:23:34 +0500

    1 and 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+function randomNumberFromOneToEinThausend() {
+    return Math.floor((Math.random() * 1000)) + 1;
+};
+let num = randomNumberFromOneToEinThausend()
 
+const repeatString = function (string, num) {
+    let returnString = '';
+    if (num < 0) return 'ERROR';
+    for (let i = 0; i < num; i++) {
+        returnString += string;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
