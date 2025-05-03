7e0a138217d1a5014204ade09d9e9b083107222f
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-03 15:05:41 +0500

    probably made with filter method
    want to also try forEach and pop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-};
+// ([1, 2, 3, 4], 3)).toEqual([1, 2, 4])
+// const removeFromArray = function (array, element) {
+//     const newArray = [];
+//     array.forEach(num => {
+//         if (num != element) {
+//             num.push(...newArray)
+//         }
+//         return newArray
+//     })
+//     return newArray
+// };
 
+const removeFromArray = function (array, elemetForDeleting) {
+    const editedArray = array.filter((element) => element !== elemetForDeleting)
+    return editedArray;
+}
+
+
+removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
