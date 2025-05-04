f0e2d0bfdcf6f4c4dfd25b9f59d0855c8444892e
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-04 23:05:07 +0500

    1-2 tests is done, but i forget about
    > positive;

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function (start, finish) {
+    Math.floor(start);
+    Math.floor(finish);
+    let sum = 0;
+    for (i = start; i <= finish; i++) {
+        sum += i;
+    }
+    return sum
 };
-
 // Do not edit below this line
 module.exports = sumAll;
