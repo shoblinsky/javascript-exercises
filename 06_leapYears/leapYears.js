1f1d0d227d985aae0727014bc1a73b07635f52dd
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-05 11:32:51 +0500

    need to fix smth

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (year % 100 & 400) {
+        return true
+    } else (year % 4)
+    return true
 };
 
 // Do not edit below this line
