52386a95fb3b727eacedeed0c3749a5dc5f409ae
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-05 11:14:58 +0500

    sumAll is done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,21 @@
 const sumAll = function (start, finish) {
-    Math.floor(start);
-    Math.floor(finish);
     let sum = 0;
+    if (start < 0 || finish < 0) return 'ERROR';
+    if ((!Number.isInteger(start) || !Number.isInteger(finish))) return 'ERROR';
+    if ((Number.isNaN(start)) || (Number.isNaN(finish))) return 'ERROR';
+    if (start > finish) {
+        for (i = finish; i <= start; i++) {
+            sum += i;
+        }
+        return sum
+    }
     for (i = start; i <= finish; i++) {
         sum += i;
     }
+
     return sum
 };
-// Do not edit below this line
+// sumAll(2, 4);
+
+// // // Do not edit below this line
 module.exports = sumAll;
