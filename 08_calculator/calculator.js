2f3cf10055573694933a860cdba3a715ee1f41be
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-05 13:15:48 +0500

    calculator is done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,42 @@
-const add = function() {
-	
+const add = function (first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function (first, second) {
+  return first - second
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  const arraySum = array.reduce(function summarizeOfArray(currentSum, currentNumber) {
+    return currentSum + currentNumber
+  }, 0);
+  return arraySum;
 };
 
-const multiply = function() {
 
+const multiply = function (array) {
+  const arrayMult = array.reduce(function summarizeOfArray(currentMult, currentNumber) {
+    return currentMult * currentNumber
+  });
+  return arrayMult;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
+  }
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let result = 1;
+  for (let i = number; i >= 1; i--) {
+    result *= i;
+  }
+  return result
 };
+
 
 // Do not edit below this line
 module.exports = {
