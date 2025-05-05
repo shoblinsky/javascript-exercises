a20050932a990b9f65a72ca619f1b3306f0a2f1e
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-05 15:16:47 +0500

    fibonacci is done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,16 +1,18 @@
 let fibonacciOnPosition = 0;
 const fibonacci = function (position) {
+    if (position < 0) return "OOPS";
+    if (position == 0) return 0;
     let firstFibonacci = 1;
     let secondFibonacci = 0;
 
     for (let i = 2; i <= (position); i++) {
+
         fibonacciOnPosition = firstFibonacci + secondFibonacci
         secondFibonacci = firstFibonacci;
         firstFibonacci = fibonacciOnPosition;
     }
-    return fibonacciOnPosition;
+    return firstFibonacci;
 };
-fibonacci(5)
 
 // Do not edit below this line
-// module.exports = fibonacci;
+module.exports = fibonacci;
