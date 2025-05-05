cb1b94a3ccc6678040f1c51574c5fd53c3dca1a8
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-05 13:40:46 +0500

    i cleared the string, but now need to compare
    symbols, one by one

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const nomenclature = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const preparedString = string.toLowerCase().split('').filter((letters) => (nomenclature.includes(letters))).join('')
+    console.log(preparedString);
+    const length = preparedString.length;
+    console.log(length);
+    for (let i = 0; i <= (length / 2); i++)
+        i === preparedString - i;
+
+    // сначала нужно очистить строку от лишних символов
+    // затем нужно получить её длину
+    // после того, как длина получена, необходимо поделить её надвое
+    // затем нужно пройтись со сравнением: символ i должен совпадать с символом с другого конца строки
+    // увеличиваем на +1
+    // если всё совпало, значит, строка -- паллиндром
 
 };
 
