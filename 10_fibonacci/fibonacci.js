e91a0094620eb49836fa7212e23cb3fec1a5937b
shoblinsky <123806946+shoblinsky@users.noreply.github.com>
2025-05-05 15:05:08 +0500

    base is done, now indeed to "OOPS"-check for below zero num.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+let fibonacciOnPosition = 0;
+const fibonacci = function (position) {
+    let firstFibonacci = 1;
+    let secondFibonacci = 0;
 
+    for (let i = 2; i <= (position); i++) {
+        fibonacciOnPosition = firstFibonacci + secondFibonacci
+        secondFibonacci = firstFibonacci;
+        firstFibonacci = fibonacciOnPosition;
+    }
+    return fibonacciOnPosition;
 };
+fibonacci(5)
 
 // Do not edit below this line
-module.exports = fibonacci;
+// module.exports = fibonacci;
